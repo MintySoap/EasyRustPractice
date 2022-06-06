d113862c82ebdafd72995ae2ef072d2e990ac86a
Hanson Liu <67702223+MintySoap@users.noreply.github.com>
2022-06-06 11:41:41 -0400

    Janky half working version with databases
    I'm still having trouble with the dynamic numbering of the rows so I'm gonna go for a less elegant approach next since right now i'm stuck at the elegant approach.

--- MediumPractice/todo/main.rs
+++ MediumPractice/todo/main.rs
@@ -1,5 +1,6 @@
 #![allow(unused_parens)]
 use std::io;
+use rusqlite::{params,Connection, Result};
 
 /*Create a command-line todo list. Users should be able to add, complete and delete items. 
 Bonus: use a database (eg SQLite) to persist todo items between programme runs.
@@ -17,10 +18,20 @@ Bonus: use a database (eg SQLite) to persist todo items between programme runs.
 */
 
 
-fn main() {
-    println!("Todo :");
-    let mut items : Vec<String> = Vec::new();
+fn main() -> Result<()>{
+    //opens a connection to the database that will hold the todo list
+    let conn = Connection::open("practice.db")?;
 
+    //creates the todo table if it doesn't exist already
+    conn.execute(
+        "create table if not exists ToDo(
+            task text primary key,
+            status text not null
+        )",
+        []
+    )?;
+
+    //Loop that gets user inputs and acts accordingly
     loop{
         //get the user's input for what they want to do with the todo list
         let mut input = String::new();
@@ -32,34 +43,43 @@ fn main() {
 
         //based on the user's input for the action, do the following action
         match input{
-            "add" => items.push(format!("{}{}",(add().trim())," (Status: incomplete)")),
-            "complete" => complete(&mut items),
-            "delete" => items = delete(&mut items),
-            "list" => list(&mut items),
+            "add" => add(&conn)?,
+            "complete" => complete(&conn)?,
+            "delete" => delete(&conn)?,
             "done" => break,
-            &_ => println!("Incorrect input, try again.")
+            &_ => error()?
         };
 
-        list(&mut items);
+        print_list(&conn)?;
     }
+
+    println!("Okay here's your final todo list:");
+    print_list(&conn)?;
+
+    //test(&conn);
+    Ok(())
 }
 
-fn add() -> String{
-
+fn add(connection : &Connection) -> Result<()>{
+    //gets the task that the user wants to add to the todo list
     let mut item = String::new();
     println!("What do you want to add?");
-
     io::stdin()
         .read_line(&mut item)
         .expect("Error");
+    let item = item.trim();
 
-    item
+    //adds the task to the database
+    //debug: doesn't work if it already exists
+    connection.execute(
+        "INSERT INTO ToDo (Task,Status) VALUES (?1,?2)",
+        params![item,"Incomplete"],
+    )?;
+
+    Ok(())
 }
 
-fn complete(items : &mut Vec<String>){
-    
-    list(items);
-
+fn complete(connection : &Connection) -> Result<()>{
     //gets the input of the item we want to mark as complete
     let mut item_num = String::new();
     println!("What do you want to mark as complete? Input the number of the item that you want to complete");
@@ -68,13 +88,18 @@ fn complete(items : &mut Vec<String>){
         .expect("Error");
     let item_num = item_num.trim().parse::<usize>().unwrap();
 
-    //marks item as complete
-    items[item_num-1] = format!("{}{}",&items[item_num-1][0..items[item_num-1].len()-11],"complete)");
+    //updates the according row so that that the task is shown as completed
+    connection.execute(
+        //"UPDATE (SELECT ROW_NUMBER() over () from ToDO) SET Status = 'Complete'",
+        "UPDATE ToDo SET Status = 'Complete' WHERE _rowid_ = ?1", //debug: need to figure out the rownum crap here too
+        params![item_num]
+    )?;
+
+    Ok(())
 }
 
-fn delete(items : &mut Vec<String>) -> Vec<String>{
-
-    list(items);
+fn delete(connection : &Connection) -> Result<()>{
+    print_list(connection);
 
     //gets the input of the item we want to mark as complete
     let mut item_num = String::new();
@@ -83,21 +108,70 @@ fn delete(items : &mut Vec<String>) -> Vec<String>{
         .read_line(&mut item_num)
         .expect("Error");
     let item_num = item_num.trim().parse::<usize>().unwrap();
-    
-    //adds all the items to the new list except the deleted item
-    let mut new_item_list : Vec<String> = Vec::new();
-    for e in (0..items.len()){
-        if(e != item_num-1){
-            new_item_list.push(items[e].clone());
-        }
-    }
-    new_item_list
+
+    //deletes the according row from the table
+    connection.execute(
+        "DELETE FROM ToDo WHERE _rowid_ = ?1",
+        params![item_num]
+    )?;
+
+    Ok(())
 }
 
-fn list(items : &mut Vec<String>){
-    //prints the todo list
-    for i in (1..items.len()+1){
-        print!("{}. ",i);
-        println!("{}",items[i-1]);
+//Note: Come back to this later because I don't understand how it works
+//debug: so basically the tasks and statuses have different rowid's so I gotta use the rownum instead
+fn print_list(conn: &Connection) -> Result<Vec<String>> {
+    let tasks = get_tasks(conn).unwrap();
+    let statuses = get_status(conn).unwrap();
+
+    for i in (0..tasks.len()){
+        println!("{}. {:?} | {:?}",i+1,tasks.get(i).unwrap(),statuses.get(i).unwrap());
     }
+
+    Ok(tasks)
+}
+
+//gets the tasks from the todo list to print
+fn get_tasks(conn: &Connection) -> Result<Vec<String>>{
+    //prepares the statement that will be executed
+    //debug:for some reason this keeps organizing the tasks in alphabetical order?
+    //solution: so for some reason doing "Select Task from ToDo" kept ordering the rows implicitly? not sure why though
+    let mut stmt = conn.prepare("SELECT * FROM ToDo")?;
+
+    //executes the statement and applies the row.get(0) function on every row so we get all of the items
+    let items = stmt.query_map([], |row| row.get(0))?;
+
+    //puts all of the items into a vector
+    let mut tasks = Vec::new();
+    for task in items {
+        tasks.push(task?);
+    }
+
+    Ok(tasks)
+}
+
+//gets the statuses of the items to print
+fn get_status(conn : &Connection) -> Result<Vec<String>>{
+    let mut stmt2 = conn.prepare("SELECT Status FROM ToDo")?;
+    let completeness = stmt2.query_map([], |row| row.get(0))?;
+    let mut statuses = Vec::new();
+    for status in completeness{
+        statuses.push(status?);
+    }
+
+    Ok(statuses)
+}
+
+fn error() -> Result<()>{
+    println!("Incorrect input, try again.");
+    Ok(())
+}
+
+fn test(connection : &Connection) ->Result<()>{
+    connection.execute(
+        "SELECT * from ToDo",
+        []
+    )?;
+
+    Ok(())
 }
